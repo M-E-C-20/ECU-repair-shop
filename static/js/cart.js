3f2485fdd009850d771fecb0a5e3fd0f48a5eee0
M-E-C-20 <mcasey8511@gmail.com>
2021-07-22 21:37:18 +0000

    Add event listener to add to cart button

---
+++ static/js/cart.js
@@ -0,0 +1,19 @@
+let updateBtns = document.getElementsByClassName('update-cart')
+
+
+// add item to cart
+for(let i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        let productId = this.dataset.product
+        let action = this.dataset.action
+        console.log('productId: ', productId, 'action: ', action)
+
+        // check if user is logged in
+        console.log('USER: ', user)
+        if(user === 'AnonymousUser'){
+            console.log('Not logged in')
+        }else{
+            console.log('User is logged in, sending data..')
+        }
+    })
+}
